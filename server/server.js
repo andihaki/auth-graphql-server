09c1b8648d696ace34b48c48a3e7f0b3f8d07740
Andi Hakim <andi.hakim.arif@gmail.com>
2018-05-14 22:14:33 +0700

    login logout button

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb://andihaki:PAssword@ds117540.mlab.com:17540/auth';
 
 // Mongoose's built in promise library is deprecated, replace it with ES2015 Promise
 mongoose.Promise = global.Promise;
